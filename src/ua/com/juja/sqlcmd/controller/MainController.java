23cd407f8739dd547cf732f49d9253fc1eb8e971
alexander.baglay <apofig@gmil.com>
2015-08-28 07:29:49 +0300

    Рефакторинг выделение команды List

--- src/ua/com/juja/sqlcmd/controller/MainController.java
+++ src/ua/com/juja/sqlcmd/controller/MainController.java
@@ -3,6 +3,7 @@ package ua.com.juja.sqlcmd.controller;
 import ua.com.juja.sqlcmd.controller.command.Command;
 import ua.com.juja.sqlcmd.controller.command.Exit;
 import ua.com.juja.sqlcmd.controller.command.Help;
+import ua.com.juja.sqlcmd.controller.command.List;
 import ua.com.juja.sqlcmd.model.DataSet;
 import ua.com.juja.sqlcmd.model.DatabaseManager;
 import ua.com.juja.sqlcmd.view.View;
@@ -21,7 +22,7 @@ public class MainController {
     public MainController(View view, DatabaseManager manager) {
         this.view = view;
         this.manager = manager;
-        this.commands = new Command[] { new Exit(view), new Help(view) };
+        this.commands = new Command[] { new Exit(view), new Help(view), new List(manager, view) };
     }
 
     public void run() {
@@ -31,8 +32,8 @@ public class MainController {
             view.write("Введи команду (или help для помощи):");
             String command = view.read();
 
-            if (command.equals("list")) {
-                doList();
+            if (commands[2].canProcess(command)) {
+                commands[2].process(command);
             } else if (commands[1].canProcess(command)) {
                 commands[1].process(command);
             } else if (commands[0].canProcess(command)) {
@@ -79,14 +80,6 @@ public class MainController {
         view.write("--------------------");
         view.write(result);
         view.write("--------------------");
-    }
-
-    private void doList() {
-        String[] tableNames = manager.getTableNames();
-
-        String message = Arrays.toString(tableNames);
-
-        view.write(message);
     }
 
     private void connectToDb() {
